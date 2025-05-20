9aa851f715be997cf330eb2dba861b24e32049d7
ndigirigijohn <jontejonhn@gmail.com>
2025-05-20 14:37:19 +0300

    rm /dist

--- dist/offchain/types/entity-registry.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MemberStatus = void 0;
-var MemberStatus;
-(function (MemberStatus) {
-    MemberStatus[MemberStatus["Active"] = 0] = "Active";
-    MemberStatus[MemberStatus["Inactive"] = 1] = "Inactive";
-    MemberStatus[MemberStatus["Suspended"] = 2] = "Suspended";
-})(MemberStatus = exports.MemberStatus || (exports.MemberStatus = {}));
